0079b959d15a1844bba4034c143b9a219e9f36b4
nl_0 <nl.imbecile@gmail.com>
2023-04-18 16:28:07 +0200

    s3hash: further work, use pydantic, support legacy

--- lambdas/s3hash/index.py
+++ lambdas/s3hash/index.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
 import asyncio
 import base64
 import contextlib
-import dataclasses
+import contextvars
+import enum
 import functools
 import hashlib
 import math
@@ -9,8 +11,9 @@ import traceback
 import typing as T
 
 import aiobotocore.config
+import aiobotocore.response
 import aiobotocore.session
-from jsonschema import Draft7Validator
+import pydantic
 import tenacity
 import types_aiobotocore_s3.type_defs as T_S3TypeDefs
 from types_aiobotocore_s3.client import S3Client
@@ -20,7 +23,39 @@ from t4_lambda_shared.utils import get_quilt_logger
 logger = get_quilt_logger()
 
 # TODO: get from env
-CONCURRENCY = 1000
+DEFAULT_CONCURRENCY = 1000
+
+
+S3: contextvars.ContextVar[S3Client] = contextvars.ContextVar("s3")
+
+
+NonEmptyStr = pydantic.constr(min_length=1, strip_whitespace=True)
+
+
+class AWSCredentials(pydantic.BaseModel):
+    key: NonEmptyStr
+    secret: NonEmptyStr
+    token: NonEmptyStr
+
+
+# Isolated for test-ability.
+def get_user_boto_session(
+    credentials: AWSCredentials,
+) -> aiobotocore.session.AioSession:
+    s = aiobotocore.session.get_session()
+    s.set_credentials(credentials.key, credentials.secret, credentials.token)
+    return s
+
+
+@contextlib.asynccontextmanager
+async def aio_context(credentials: AWSCredentials, concurrency: pydantic.PositiveInt):
+    session = get_user_boto_session(credentials)
+    config = aiobotocore.config.AioConfig(max_pool_connections=concurrency)
+
+    async with session.create_client("s3", config=config) as s3:
+        s3_token = S3.set(s3)
+        yield
+        S3.reset(s3_token)
 
 
 class S3hashException(Exception):
@@ -29,62 +64,45 @@ class S3hashException(Exception):
         self.name = name
         self.context = context
 
-    def asdict(self):
+    def dict(self):
         return {"name": self.name, "context": self.context}
 
 
-CHECKSUM_TYPE = "QuiltChecksumSHA256"
-LEGACY_CHECKSUM_TYPE = "SHA256"
+class ChecksumType(enum.Enum):
+    MP = "QuiltMultipartSHA256"
+    SP = "SHA256"
 
 
-@dataclasses.dataclass
-class Checksum:
-    value: bytes
-    part_count: T.Optional[int]  # None means legacy mode
-
-    @property
-    def is_legacy(self):
-        return self.part_count is None
-
-    @property
-    def type(self):
-        return CHECKSUM_TYPE if not self.is_legacy else LEGACY_CHECKSUM_TYPE
-
-    @property
-    def value_str(self):
-        if self.is_legacy:
-            return self.value.hex()
-
-        b64 = base64.b64encode(self.value).decode()
-        return f"{b64}-{self.part_count}" if self.part_count else b64
+class Checksum(pydantic.BaseModel):
+    # TODO: make sure this serializes to str
+    type: ChecksumType
+    value: str
 
     @classmethod
     def singlepart(cls, value: bytes):
-        return cls(value, 0)
+        return cls(value=value.hex(), type=ChecksumType.SP)
 
     @classmethod
     def multipart(cls, parts: T.Sequence[bytes]):
-        return cls(hash_parts(parts), len(parts))
+        hash_list = hash_parts(parts)
+        b64 = base64.b64encode(hash_list).decode()
+        value = f"{b64}-{len(parts)}"
+        return cls(value=value, type=ChecksumType.MP)
 
     @classmethod
-    def legacy(cls, value: bytes):
-        return cls(value, None)
+    def for_parts(cls, checksums: T.Sequence[bytes], defs: T.Sequence[PartDef]):
+        if defs == PARTS_SINGLE:
+            return cls.singlepart(checksums[0])
+        return cls.multipart(checksums)
 
     def __str__(self):
-        return f"{self.type}:{self.value_str}"
+        return f"{self.type}:{self.value}"
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self!s})"
 
-    def asdict(self):
-        return {
-            "type": self.type,
-            "value": self.value_str,
-        }
 
-
-@dataclasses.dataclass
-class S3ObjectSource:
+class S3ObjectSource(pydantic.BaseModel):
     bucket: str
     key: str
     version: str
@@ -98,8 +116,7 @@ class S3ObjectSource:
         }
 
 
-@dataclasses.dataclass
-class S3ObjectDestination:
+class S3ObjectDestination(pydantic.BaseModel):
     bucket: str
     key: str
 
@@ -111,8 +128,7 @@ class S3ObjectDestination:
         }
 
 
-@dataclasses.dataclass
-class MPURef:
+class MPURef(pydantic.BaseModel):
     bucket: str
     key: str
     id: str
@@ -125,8 +141,6 @@ class MPURef:
             "UploadId": self.id,
         }
 
-
-TMP_KEY = ".quilt/checksum-tmp"
 
 # 8 MiB -- boto3 default: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/customizations/s3.html#boto3.s3.transfer.TransferConfig
 MIN_PART_SIZE = 1024**2 * 8
@@ -149,7 +163,10 @@ def get_part_size(file_size: int) -> T.Optional[int]:
     return part_size
 
 
-def get_existing_checksum(src: S3ObjectSource) -> T.Optional[Checksum]:
+async def get_existing_checksum(
+    location: S3ObjectSource,
+    parts: T.Sequence[PartDef],
+) -> T.Optional[Checksum]:
     # get object attributes and see if part sizes and checksum algo matches our standard
     # return the existing checksum if it's compliant
     # otherwise return None
@@ -160,263 +177,167 @@ def hash_parts(parts: T.Sequence[bytes]) -> bytes:
     return hashlib.sha256(b"".join(parts)).digest()
 
 
-@dataclasses.dataclass
-class PartDef:
+class PartDef(pydantic.BaseModel):
     part_number: int
-    part_size: T.Optional[int]
-    offset: T.Optional[int]
+    range: T.Optional[T.Tuple[int, int]]
 
     @property
     def boto_args(self):
-        res: T.Dict[str, T.Any] = {
-            "PartNumber": self.part_number,
-        }
-        if self.part_size is not None and self.offset is not None:
-            res[
-                "CopySourceRange"
-            ] = f"bytes={self.offset}-{self.offset + self.part_size - 1}"
-        return res
+        args: T.Dict[str, T.Any] = {"PartNumber": self.part_number}
+        if self.range:
+            args["CopySourceRange"] = f"bytes={self.range[0]}-{self.range[1]}"
+        return args
+
+    @classmethod
+    def for_range(cls, part_number: int, offset: int, size: int):
+        return cls(
+            part_number=part_number,
+            range=(offset, offset + size - 1),
+        )
 
 
-def enumerate_parts(total_size: int, part_size: T.Optional[int]):
+PARTS_SINGLE = [PartDef(part_number=1, range=None)]
+
+
+async def get_parts_for_location(location: S3ObjectSource) -> T.List[PartDef]:
+    total_size = (await S3.get().head_object(**location.boto_args))["ContentLength"]
+    part_size = get_part_size(total_size)
+
+    # single-part upload
     if part_size is None:
-        # single-part upload
-        yield PartDef(part_number=1, part_size=None, offset=None)
-        return
+        return PARTS_SINGLE
 
     # multipart upload
     offset = 0
     part_number = 1
+    parts = []
     while offset < total_size:
         actual_part_size = min(part_size, total_size - offset)
-        yield PartDef(
-            part_number=part_number, part_size=actual_part_size, offset=offset
+        parts.append(
+            PartDef.for_range(
+                part_number=part_number,
+                offset=offset,
+                size=actual_part_size,
+            )
         )
         offset += actual_part_size
         part_number += 1
 
+    return parts
 
-async def get_writable_location(
-    s3: S3Client,
+
+@tenacity.retry
+async def upload_part(
+    mpu: MPURef,
     src: S3ObjectSource,
-    dst: T.Optional[S3ObjectDestination],
-) -> S3ObjectDestination:
-    if dst is None:
-        # find a location writable by the current user if it's not provided
-        # default to bucket/.quilt/tmp?
-        # XXX: consider using service bucket
-        dst = S3ObjectDestination(bucket=src.bucket, key=TMP_KEY)
-    return dst
-
-
-async def compute_checksum_remote(
-    s3: S3Client,
-    src: S3ObjectSource,
-    dst: T.Optional[S3ObjectDestination] = None,
-) -> T.Tuple[Checksum, MPURef]:
-    dst_task = asyncio.create_task(get_writable_location(s3, src, dst))
-    src_head_task = asyncio.create_task(s3.head_object(**src.boto_args))
-
-    dst = await dst_task
-    # initiate a multipart upload to the given destination
-    upload_data = await s3.create_multipart_upload(
-        **dst.boto_args,
-        ChecksumAlgorithm="SHA256",
+    part: PartDef,
+) -> bytes:
+    res = await S3.get().upload_part_copy(
+        **mpu.boto_args,
+        **part.boto_args,
+        CopySource=src.boto_args,
     )
+    cs = res["CopyPartResult"].get("ChecksumSHA256")  # base64-encoded
+    assert cs is not None
+    return base64.b64decode(cs)
 
-    upload_ref = MPURef(bucket=dst.bucket, key=dst.key, id=upload_data["UploadId"])
 
-    # copy (concurrently) all the parts from src with the proper part size / checksum settings
-    total_size = (await src_head_task)["ContentLength"]
-    part_size = get_part_size(total_size)
-    single_part = part_size is None
-
-    @tenacity.retry
-    async def _upload_part(part_def: PartDef):
-        res = await s3.upload_part_copy(
-            **upload_ref.boto_args,
-            **part_def.boto_args,
-            CopySource=src.boto_args,
-        )
-        cs = res["CopyPartResult"].get("ChecksumSHA256")  # base64-encoded
-        assert cs is not None
-        return base64.b64decode(cs)
-
-    uploads = [_upload_part(p) for p in enumerate_parts(total_size, part_size)]
+async def compute_part_checksums(
+    mpu: MPURef,
+    location: S3ObjectSource,
+    parts: T.Sequence[PartDef],
+) -> T.List[bytes]:
+    uploads = [upload_part(mpu, location, p) for p in parts]
     # retain the checksums for all the parts to compute the final checksum
     checksums: T.List[bytes] = await asyncio.gather(*uploads)
 
-    print("retry stats:", _upload_part.retry.statistics)
+    # XXX: push cloudwatch metrics?
+    print("retry stats:", upload_part.retry.statistics)
 
-    if single_part:
-        checksum = Checksum.singlepart(checksums[0])
-    else:
-        checksum = Checksum.multipart(checksums)
-
-    # return the calculated checksum and the upload reference
-    return checksum, upload_ref
+    return checksums
 
 
-async def get_or_compute_checksum_remote(s3: S3Client, loc: S3ObjectSource) -> Checksum:
-    checksum = get_existing_checksum(loc)
+async def create_mpu(target: S3ObjectDestination) -> MPURef:
+    # initiate a multipart upload to the given destination
+    upload_data = await S3.get().create_multipart_upload(
+        **target.boto_args,
+        ChecksumAlgorithm="SHA256",
+    )
 
-    if checksum is None:
-        checksum, upload_ref = await compute_checksum_remote(s3, loc)
-
-        try:
-            await s3.abort_multipart_upload(**upload_ref.boto_args)
-        except Exception:
-            logger.exception("Error aborting MPU")
-
-        # parts_res = await s3.list_parts(**upload_ref.boto_args)
-        # print('MPU Parts:', parts_res["Parts"])
-        # parts = [
-        #     {
-        #         "PartNumber": p["PartNumber"],
-        #         "ChecksumSHA256": p["ChecksumSHA256"],
-        #         "ETag": p["ETag"],
-        #     } for p in parts_res["Parts"]
-        # ]
-        # print('complete args', dict(
-        #     **upload_ref.boto_args,
-        #     MultipartUpload={"Parts": parts},
-        #     ChecksumSHA256=f"{checksum.value}-{checksum.part_count}",
-        # ))
-        # res = await s3.complete_multipart_upload(
-        #     **upload_ref.boto_args,
-        #     MultipartUpload={"Parts": parts},
-        #     ChecksumSHA256=checksum.value_str,
-        # )
-
-    return checksum
+    return MPURef(bucket=target.bucket, key=target.key, id=upload_data["UploadId"])
 
 
-async def compute(loc: S3ObjectSource, legacy: bool) -> Checksum:
-    assert user_boto_session is not None
-    config = aiobotocore.config.AioConfig(max_pool_connections=CONCURRENCY)
-    async with user_boto_session.create_client("s3", config=config) as s3:
-        return await get_or_compute_checksum_remote(s3, loc)
-    # TODO: support legacy checksums
+STREAM_CHUNK_SIZE = 128 * 2**10
 
 
-# Isolated for test-ability.
-def get_user_boto_session(aws_access_key_id, aws_secret_access_key, aws_session_token):
-    s = aiobotocore.session.get_session()
-    s.set_credentials(aws_access_key_id, aws_secret_access_key, aws_session_token)
-    return s
+async def compute_checksum_stream(location: S3ObjectSource) -> Checksum:
+    resp = await S3.get().get_object(**location.boto_args)
+    async with resp["Body"] as stream:
+        stream: aiobotocore.response.StreamingBody
+        hashobj = hashlib.sha256()
+        async for chunk in stream.iter_chunks(chunk_size=STREAM_CHUNK_SIZE):
+            hashobj.update(chunk)
+        return Checksum.singlepart(hashobj.digest())
 
 
-user_boto_session: T.Optional[aiobotocore.session.AioSession] = None
+class ChecksumResult(pydantic.BaseModel):
+    checksum: Checksum
+    mpu: T.Optional[MPURef] = None
 
 
-@contextlib.contextmanager
-def setup_user_boto_session(session: aiobotocore.session.AioSession):
-    global user_boto_session
-    user_boto_session = session
-    try:
-        yield user_boto_session
-    finally:
-        user_boto_session = None
-
-
-CREDENTIALS_SCHEMA = {
-    "$schema": "http://json-schema.org/draft-07/schema#",
-    "id": "https://quiltdata.com/aws-credentials/1",
-    "type": "object",
-    "properties": {
-        "aws_access_key_id": {"type": "string", "pattern": "^.+$"},
-        "aws_secret_access_key": {"type": "string", "pattern": "^.+$"},
-        "aws_session_token": {"type": "string", "pattern": "^.+$"},
-    },
-    "required": [
-        "aws_access_key_id",
-        "aws_secret_access_key",
-        "aws_session_token",
-    ],
-}
-
-
-def auth(f):
-    validator = Draft7Validator(CREDENTIALS_SCHEMA)
-
-    @functools.wraps(f)
-    def wrapper(event):
-        credentials = event.get("credentials")
-        # TODO: collect all errors
-        ex = next(validator.iter_errors(credentials), None)
-        if ex is not None:
-            raise S3hashException("InvalidCredentials", {"details": ex.message})
-
-        with setup_user_boto_session(get_user_boto_session(**credentials)):
-            return f(event.get("params"))
-
-    return wrapper
-
-
-def exception_handler(f):
+# XXX: need a consistent way to serialize / deserialize exceptions
+def lambda_handler(f):
+    # TODO: catch validation errors
+    # raise S3hashException("InvalidCredentials", {"details": ex.message})
+    # raise S3hashException("InvalidInputParameters", {"details": ex.message})
     @functools.wraps(f)
     def wrapper(event, _context):
         try:
-            return {"result": f(event)}
+            return {"result": asyncio.run(f(**event)).dict()}
         except S3hashException as e:
             traceback.print_exc()
-            return {"error": e.asdict()}
+            return {"error": e.dict()}
 
     return wrapper
-
-
-def get_schema_validator(schema):
-    iter_errors = Draft7Validator(schema).iter_errors
-
-    def validator(data):
-        ex = next(iter_errors(data), None)
-        # TODO: collect all errors
-        if ex is not None:
-            raise S3hashException("InvalidInputParameters", {"details": ex.message})
-        return data
-
-    return validator
-
-
-def json_api(schema):
-    validator = get_schema_validator(schema)
-
-    def innerdec(f):
-        @functools.wraps(f)
-        def wrapper(params):
-            validator(params)
-            return f(params)
-
-        return wrapper
-
-    return innerdec
-
-
-PARAMS_SCHEMA = {
-    "type": "object",
-    "properties": {
-        "location": {
-            "type": "object",
-            "properties": {
-                "bucket": {"type": "string"},
-                "key": {"type": "string"},
-                "version": {"type": "string"},
-            },
-            "required": ["bucket", "key", "version"],
-        },
-        "legacy": {"type": "boolean"},
-    },
-    "required": ["location"],
-}
 
 
 # XXX: move decorators to shared?
 # XXX: move reusable models/dataclasses to shared?
-@exception_handler
-@auth
-@json_api(PARAMS_SCHEMA)
-def lambda_handler(params):
-    loc = S3ObjectSource(**params["location"])
-    legacy = params.get("legacy", False)
-    checksum = asyncio.run(compute(loc, legacy))
-    return checksum.asdict()
+@lambda_handler
+@pydantic.validate_arguments
+async def compute_checksum(
+    credentials: AWSCredentials,
+    location: S3ObjectSource,
+    target: T.Optional[S3ObjectDestination] = None,
+    keep_mpu: bool = False,
+    legacy: bool = False,
+    concurrency: pydantic.PositiveInt = DEFAULT_CONCURRENCY,
+) -> ChecksumResult:
+    async with aio_context(credentials, concurrency):
+        if legacy and target is None:
+            checksum = await compute_checksum_stream(location)
+            return ChecksumResult(checksum=checksum)
+
+        # XXX: raise validation error?
+        assert target is not None, "target must be specified for new-style checksums"
+
+        part_defs = PARTS_SINGLE if legacy else await get_parts_for_location(location)
+
+        checksum = await get_existing_checksum(location, part_defs)
+        if checksum is not None:
+            return ChecksumResult(checksum=checksum)
+
+        mpu = await create_mpu(target)
+
+        part_checksums = await compute_part_checksums(mpu, location, part_defs)
+
+        checksum = Checksum.for_parts(part_checksums, part_defs)
+
+        if not keep_mpu:
+            try:
+                await S3.get().abort_multipart_upload(**mpu.boto_args)
+                mpu = None
+            except Exception:
+                logger.exception("Error aborting MPU")
+
+        return ChecksumResult(checksum=checksum, mpu=mpu)
